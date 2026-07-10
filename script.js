65d6a41e5daa40dec603c03576e858b94304ec65
Arnav Govil <160424969+mushyalienstar@users.noreply.github.com>
2026-07-10 15:57:35 -0400

    added sticky navigation so menu still displayed while scrolling

--- script.js
+++ script.js
@@ -106,6 +106,14 @@ nav.addEventListener('mouseover', handleHover.bind(0.5));
 
 nav.addEventListener('mouseout', handleHover.bind(1));
 
+// Sticky navigation
+const initialCoords = section1.getBoundingClientRect();
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY > initialCoords.top) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+});
+
 /////////////////////////////////////
 /////////////////////////////////////
 /////////////////////////////////////
